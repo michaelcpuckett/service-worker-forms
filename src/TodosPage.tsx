5071204833fc49e731ba1140c59171891025fd33
Michael Puckett <michaelcpuckett@gmail.com>
2023-11-02 19:36:42 -0400

    Focus management

--- src/TodosPage.tsx
+++ src/TodosPage.tsx
@@ -9,17 +9,19 @@ export function TodosPage(props: React.PropsWithChildren<{ todos?: Todo[]; refer
         <script src="/scroll-restoration.js"></script>
       </head>
       <body>
-        <h1>Todos</h1>
-        <form action="" method="POST">
-          <input type="hidden" name="action" value="/api/todos" />
-          <input type="hidden" name="method" value="POST" />
-          <label>
-            Title
-            <input autoFocus={props.referrer.state === 'ADD_TODO' ? true : undefined} type="text" name="title" placeholder="Title" />
-          </label>
-          <button type="submit">Add</button>
-        </form>
-        <TodosTableView todos={props.todos || []} referrer={props.referrer} />
+        <main>
+          <h1>Todos</h1>
+          <form action="" method="POST">
+            <input type="hidden" name="action" value="/api/todos" />
+            <input type="hidden" name="method" value="POST" />
+            <label>
+              Title
+              <input autoFocus={((props.referrer.state === 'DELETE_TODO' && props.todos?.length === 0) || (props.referrer.state === 'ADD_TODO')) ? true : undefined} type="text" name="title" placeholder="Title" />
+            </label>
+            <button type="submit">Add</button>
+          </form>
+          <TodosTableView todos={props.todos || []} referrer={props.referrer} />
+        </main>
       </body>
     </html>
   );
